9f0a4f18518972b16e9b86bc4864e26b614479d1
striker-samuel <samuel.martin.work99@gmail.com>
2018-12-27 18:38:36 +0100

    Added VDM, not ready to go yet

---
+++ Assets/Assets/Scripts/starter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starter : MonoBehaviour {
+
+    terrainScript customTerrain = new terrainScript();
+
+    public int detail = 1;
+
+    // Use this for initialization
+
+    void Start()
+    {
+        int chunks = detail*4;
+        //Texture2D white = new Texture2D(180,180);
+        //terrainScript customTerrain = GetComponent<terrainScript>();
+        Texture2D terratex = Resources.Load("dspmap", typeof(Texture2D)) as Texture2D;
+        //Debug.Log(white.GetPixel(1, 1));
+        int id = 1;
+
+        for (int i = 0; (i + 1) <= chunks; i++)
+        {
+            for (int j = 0; (j + 1) <= chunks; j++)
+            {
+                customTerrain.Generate(terratex,chunks,(i) * 64 + 0, (j) * 64 + 0, id);
+                id++;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/Assets/Scripts/terrainScript.cs
+++ Assets/Assets/Scripts/terrainScript.cs
@@ -2,129 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class terrainScript : MonoBehaviour {
 
-
+    
 
     public float frecuency = 2;
+    private float density = 1;
+
+    public int xSize;
+    public int zSize;
     
-    public int xSize = 60;
-    public int zSize = 60;
-    public float density = 1;
     Vector3[] vertices;
     Vector2[] uvs;
 
-    public int chunks = 5;
-
-    // Use this for initialization
-    void Awake() {
-
-
-        
-
-    #if UNITY_EDITOR
-        UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
-	#endif
-
-        
-        transform.position = new Vector3((-xSize / 2), transform.position.y,(- zSize / 2));
-
-        //for (int jx=0;jx<=chunks;jx++) {
-        //    for (int jz = 0;jz <= chunks;jz++) {
-        //        Generate(xSize*jx, zSize*jz);
-        //    }
-        //}
-
-        Generate();
-
+    private void Start()
+    {
     }
 
+    void Awake() {
+    #if UNITY_EDITOR
+        UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
+    #endif
+    }
+
+
+    private Vector3 displacement (int x, int z, Texture2D dispMap)
+    {
+        Vector3 dspVector = new Vector3();
+
+        var color =  dispMap.GetPixel(x,z);
+        dspVector.x = color.r - 0.5f;
+        dspVector.y = color.g - 0.5f;
+        dspVector.z = color.b - 0.5f;
+
+
+        Debug.Log(dspVector.x);
+        return dspVector;
+    }
 
     private float ynoise(float x, float z) {
 
-        return Mathf.PerlinNoise(x/xSize* frecuency, z/zSize*frecuency)*1f;
+        return Mathf.PerlinNoise(x / xSize * frecuency, z / zSize * frecuency) * 1f;
         //return Random.Range(0,1);
     }
-        
 
 
-
-    //[ExecuteInEditMode]
-    //public void Generate()
-    //{
-    //    Mesh terrainGeo = new Mesh();
-
-        
-
-    //    GetComponent<MeshFilter>().mesh = terrainGeo= new Mesh();
-    //    terrainGeo.name = "Terrain";
-
-    //    vertices = new Vector3[(xSize + 1) * (zSize + 1)];
-    //    for (int i = 0, z = 0; z <= zSize; z++)
-    //    {
-    //        for (int x = 0; x <= xSize; x++, i++)
-    //        {
-    //            float height = ynoise(x,z);
-    //            vertices[i] = new Vector3(x,height*10, z);
-    //        }
-    //    }
-    //    terrainGeo.vertices = vertices;
-
-    //    int[] triangles = new int[xSize * zSize * 6];
-    //    for (int ti = 0, vi = 0, z = 0; z < zSize; z++, vi++)
-    //    {
-    //        for (int x = 0; x < xSize; x++, ti += 6, vi++)
-    //        {
-    //            triangles[ti] = vi;
-    //            triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-    //            triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
-    //            triangles[ti + 5] = vi + xSize + 2;
-    //        }
-    //    }
-
-    //    uvs = new Vector2[vertices.Length];
-
-    //    for (int i = 0, z = 0; z <= zSize; z++)
-    //    {
-    //        for (int x = 0; x <= xSize; x++, i++)
-    //        {
-    //            uvs[i] = new Vector2((float)x / xSize,(float) z / zSize);
-    //        }
-    //    }
-
-    //    terrainGeo.uv = uvs;
-    //    terrainGeo.triangles = triangles;
-    //    terrainGeo.RecalculateBounds();
-    //    terrainGeo.RecalculateNormals();
+    public Vector3[] vertexrqt(Vector3[] vertexarray)
+    {
+        for (int i = 0;i <= vertexarray.Length;i++)
+        {
+            for (int j = 0; j <= vertexarray.Length; j++)
+            {
+                
+            }
+        }
 
 
-
-    //    //Assigning MeshCollider
-    //    MeshCollider meshc = gameObject.AddComponent<MeshCollider>();
-    //    meshc.sharedMesh = terrainGeo;
-        
-
-
-    //}
+        Vector3[] finalvertex = vertexarray;
+        return finalvertex;
+    }
 
     [ExecuteInEditMode]
-    public void Generate(int xinit = 0, float zinit = 0)
+    public void Generate(Texture2D dspmap,int chunks,int xinit = 0, int zinit = 0, int terrainID = 0)
     {
-        
-       
+
+
+        //Debug.Log(dspmap.width);
+        xSize = 64;// dspmap.width/chunks;
+        zSize = 64;//dspmap.height/chunks;
+        //Debug.Log(xSize);
+        //dspmap.Resize(dspmap.width / 16 * chunks, dspmap.width / 10 * chunks);
+        //dspmap.Resize(64*chunks,64*chunks);
+        dspmap.Resize(256, 256);
+        Debug.Log(dspmap.width);
+
+        GameObject tile = new GameObject();
+        tile.name = "Terrain Tile " + xinit +" " + zinit;        
+        tile.AddComponent <MeshRenderer>();
 
         Mesh terrainGeo = new Mesh();
+        terrainGeo = new Mesh();
+        terrainGeo.name = "Terrain tile " + terrainID;
 
-        GetComponent<MeshFilter>().mesh = terrainGeo = new Mesh();
-        terrainGeo.name = "Terrain";
+        var tilepos = tile.transform.position;
 
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
         for (int i = 0, z = 0; z <= zSize; z++)
         {
             for (int x = 0; x <= xSize; x++, i++)
             {
-                float height = ynoise(x, z);
-                vertices[i] = new Vector3(x+xinit, height * 10, z+zinit);
+                Vector3 startpos = new Vector3(x,0,z);
+                float height = ynoise(xinit+x, zinit+z);
+                //float height = Random.Range(0,3);
+                //vertices[i] = new Vector3(x + xinit, height * 10, z + zinit);
+                //vertices[i] = new Vector3(x + xinit, 0, z + zinit);
+                Vector3 dspResult = displacement(x+xinit, z+zinit, dspmap);
+                vertices[i] = startpos + dspResult*10;
             }
         }
         terrainGeo.vertices = vertices;
@@ -151,20 +125,23 @@ public class terrainScript : MonoBehaviour {
             }
         }
 
+        tile.transform.position = new Vector3(xinit,0,zinit);
+
+        //Assigning mesh data
         terrainGeo.uv = uvs;
         terrainGeo.triangles = triangles;
         terrainGeo.RecalculateBounds();
         terrainGeo.RecalculateNormals();
 
-
-
-        //Assigning MeshCollider
-        MeshCollider meshc = gameObject.AddComponent<MeshCollider>();
+        //Assigning components to GO.
+        MeshFilter tilecomplete = tile.AddComponent<MeshFilter>();
+        tilecomplete.mesh = terrainGeo;
+        MeshCollider meshc = tile.AddComponent<MeshCollider>();
         meshc.sharedMesh = terrainGeo;
 
-
-
-    }
+        Material mat = Resources.Load("terrainMat", typeof(Material)) as Material;
+        tile.GetComponent<Renderer>().material = mat;
+ }
 
 
     void Update () {
